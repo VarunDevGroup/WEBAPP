d0d254dd4a143bb5f1a069943c08cc88da1192ec
VarunDevGroup <varun.v.sharma@outlook.com>
2025-05-04 14:15:46 +0530

    google login part2

--- responsive-login-app/src/js/scripts.js
+++ responsive-login-app/src/js/scripts.js
@@ -53,4 +53,12 @@ document.addEventListener("DOMContentLoaded", function() {
         // Decode the JWT token and handle the user login
         // You can send the token to your backend for verification
     };
+
+    function onSignIn(googleUser) {
+        var profile = googleUser.getBasicProfile();
+        console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+        console.log('Name: ' + profile.getName());
+        console.log('Image URL: ' + profile.getImageUrl());
+        console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+      }
 });
